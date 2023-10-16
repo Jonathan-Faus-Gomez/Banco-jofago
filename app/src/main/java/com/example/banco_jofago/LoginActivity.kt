a07ffed6dec9db900fe6fdecab1fa83bdac3419d
Jonathan Faus <jonfaugom@alu.edu.gva.es>
2023-10-16 16:06:38 +0200

    ya

--- app/src/main/java/com/example/banco_jofago/LoginActivity.kt
+++ app/src/main/java/com/example/banco_jofago/LoginActivity.kt
@@ -1,4 +1,32 @@
 package com.example.banco_jofago
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.banco_jofago.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.botonEntrar.setOnClickListener {
+            val dni = binding.usuario.editText?.text.toString()
+            val password = binding.password.editText?.text.toString()
+
+            if (dni.isEmpty() || password.isEmpty()) {
+                binding.usuario.error="Por favor rellene ambos campos"
+                binding.password.error="Por favor rellene ambos campos"
+            } else {
+                val intent = Intent(this, MainActivity::class.java )
+                intent.putExtra("dni",dni)
+                startActivity(intent)
+            }
+        }
+
+        binding.botonSalir.setOnClickListener {
+            finish()
+        }
+    }
 }
--- app/src/main/java/com/example/banco_jofago/MainActivity.kt
+++ app/src/main/java/com/example/banco_jofago/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.banco_jofago
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.banco_jofago.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val dni = intent.getStringExtra("dni")
+
+        // Actualiza el mensaje de bienvenida con el número de DNI
+        binding.bienvenido.text = "Bienvenido/a, $dni"
     }
 }
