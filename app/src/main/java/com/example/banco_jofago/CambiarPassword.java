119dd9d5810d0a8e49011a6b58db2f21cde1116b
Jonathan Faus <jonfaugom@alu.edu.gva.es>
2023-10-18 19:56:04 +0200

    tre letra

--- app/src/main/java/com/example/banco_jofago/CambiarPassword.java
+++
@@ -1,8 +0,0 @@
-package com.example.banco_jofago;
-
-public class CambiarPassword {
- // como en login utilizando activity_cambiar_password.xml
-    //salir -> main activity
-    //coincidan 2 contraseñas sino sale error
-    //no vacias
-}
---
+++ app/src/main/java/com/example/banco_jofago/CambiarPassword.kt
@@ -0,0 +1,13 @@
+package com.example.banco_jofago;
+
+import com.example.banco_jofago.databinding.ActivityCambiarPasswordBinding;
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+public class CambiarPassword : AppCompatActivity(){
+    private lateinit var binding: ActivityCambiarPasswordBinding
+    // como en login utilizando activity_cambiar_password.xml
+    //salir -> main activity
+    //coincidan 2 contraseñas sino sale error
+    //no vacias
+}
