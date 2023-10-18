fa1a26e990c44107603940f8d2bc45f1f18c7c77
Jonathan Faus <jonfaugom@alu.edu.gva.es>
2023-10-18 19:49:36 +0200

    saico boni

--- app/src/main/java/com/example/banco_jofago/CambiarPassword.java
+++ app/src/main/java/com/example/banco_jofago/CambiarPassword.java
@@ -1,5 +1,8 @@
 package com.example.banco_jofago;
 
 public class CambiarPassword {
-
+ // como en login utilizando activity_cambiar_password.xml
+    //salir -> main activity
+    //coincidan 2 contraseñas sino sale error
+    //no vacias
 }
